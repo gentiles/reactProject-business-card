9fbddbcb2f5d0547e1f3266527e6df8d133e127b
erwin <erwinmarin05@gmail.com>
2024-05-16 01:13:05 -0700

    initial
    initial

---
+++ components/About.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function About(){
+    return(
+       
+            <div className="main-content">
+                <h2 className="more-info">About</h2>
+                <p className="paragraph">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+                <h2 className="more-info">Interest</h2>
+                <p className="paragraph">MMA fan. Music scholar. Reader. Internet fanatic. Content creation. Open source. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            </div>
+  
+        
+    )
+}
